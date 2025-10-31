02e0e85b4e46b5e596aa96b2df58e5a4293bae14
sina kaleji <sinakalejii@gmail.com>
2025-10-31 20:18:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5238583829-ed2f1",
+  "appId": "1:409022876212:web:4c44abf10e5ea76aa39861",
+  "apiKey": "AIzaSyB-h8oKscK28MMt7UDK3_xpF-CTLT1wS_g",
+  "authDomain": "studio-5238583829-ed2f1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "409022876212"
+};
