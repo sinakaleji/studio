bae7f654befb6a89d6a64581554eafa046d9909c
Sina Kaleji <sinakaleji@gmail.com>
2025-11-04 20:33:38 +0000

    کل پروژه را کاملا آفلاین کن

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <LayoutProvider>{children}</LayoutProvider>
