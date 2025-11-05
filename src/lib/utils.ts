1528c2d2848ddc14463e34aab3d1dbaa05f7b1cf
Sina Kaleji <sinakaleji@gmail.com>
2025-11-05 10:34:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function toPersianDigits(n: number | string) {
+  if (n === null || n === undefined) {
+    return "";
+  }
   const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
   return n.toString().replace(/\d/g, (x) => persianDigits[parseInt(x)]);
 }
