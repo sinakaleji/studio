9fd594e184772589c84f0ec7bb16fc510bd1bcba
Sina Kaleji <sinakaleji@gmail.com>
2025-11-05 19:52:51 +0000

    مشکل من سرجایش مانده و این مرا کلافه کرده ، وقتی در این محیط اجرا میشود

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type {Config} from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -97,5 +96,3 @@ const config: Config = {
   },
   plugins: [require('tailwindcss-animate')],
 };
-
-export default config;
